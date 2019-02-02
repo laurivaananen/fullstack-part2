e2b42361c09ef05a2d69ef91f3ecfa273a66a002
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2019-02-02 12:18:20 +0200

    Initial commit

---
+++ kurssitiedot/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = props =>
+  <h1>{props.course}</h1>
+
+const Total = props => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+
+  return <p>yhteensä {total} tehtävää</p>
+}
+  
+
+const Part = props =>
+  <p>{props.part.name} {props.part.exercises}</p>
+
+const Content = props => (
+  <div>
+    {props.parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
